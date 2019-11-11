bd53422fe3ec29bc3c61b58cec4c7ed067c75810
John D <john.davenport71@gmail.com>
2019-11-10 20:09:57 -0500

    Good progress made on functionality. Form fields are locked in pending any changes. Next step is incorporating design.

--- src/form.js
+++ src/form.js
@@ -7,7 +7,9 @@ class Form extends Component {
         this.state = {
             zipcode: '',
             animaltype: '',
-            breed: ''
+            breed: '',
+            gender: '',
+            distance: 100
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -29,10 +31,15 @@ class Form extends Component {
         } 
         if(this.state.zipcode) {
             url += 'location=' + this.state.zipcode + '&';
+            url += 'distance=' + this.state.distance + '&';
         }
         if(this.state.breed) {
             url += 'breed=' + this.state.breed + '&';
         }
+        if(this.state.gender) {
+            url += 'gender=' + this.state.gender + '&';
+        }
+
         
        fetch(url, {
           method: 'get',
@@ -54,34 +61,64 @@ class Form extends Component {
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="zipcode">Zipcode:</label>
                     <input type="text" id="zipcode" name="zipcode" value={this.state.zipcode} onChange={this.handleChange} />
+                    <br></br>
+                    {
+                        this.state.zipcode ? 
+                        <>
+                            <label htmlFor="distance">Distance (mi):</label>
+                            <input type="number" id="distance" name="distance" value={this.state.distance} onChange={this.handleChange} />
+                            <br></br>
+                        </>
+                        :
+                        ''
+                    }
+                    
                     <label htmlFor="animaltype">Animal Type</label>
                     <select id="animaltype" name="animaltype" value={this.state.animaltype} onChange={this.handleChange} >
                         <option value="">All</option>
                         <option value="cat">Cats</option>
                         <option value="dog">Dogs</option>
                     </select>
+                    <br></br>
                     {
                         this.state.animaltype === 'cat' ? 
-                        <select id="cat-breed" name="breed" value={this.state.breed} onChange={this.handleChange}>
-                            <option value="">Select a Breed (optional)</option>
-                            <option value="Calico">Calico</option>
-                            <option value="Domestic Short Hair">Domestic Short Hair</option>
-                            <option value="Domestic Long Hair">Domestic Long Hair</option>
-                            <option value="Maine Coon">Maine Coon</option>
-                            <option value="Tabby">Tabby</option>
-                            <option value="Tuxedo">Tuxedo</option>
-                        </select>
-                        :
-                        <select id="dog-breed" name="breed" value={this.state.breed} onChange={this.handleChange}>
-                            <option value="">Select a Breed (optional)</option>
-                            <option value="Beagle">Beagle</option>
-                            <option value="Boxer">Boxer</option>
-                            <option value="Chihuahua">Chihuahua</option>
-                            <option value="Retriever">Retriever</option>
-                            <option value="Shepherd">Shepherd</option>
-                            <option value="Terrier">Terrier</option>
-                        </select>
+                        <>
+                            <select id="cat-breed" name="breed" value={this.state.breed} onChange={this.handleChange}>
+                                <option value="">Select a Breed (optional)</option>
+                                <option value="Calico">Calico</option>
+                                <option value="Domestic Short Hair">Domestic Short Hair</option>
+                                <option value="Domestic Long Hair">Domestic Long Hair</option>
+                                <option value="Maine Coon">Maine Coon</option>
+                                <option value="Tabby">Tabby</option>
+                                <option value="Tuxedo">Tuxedo</option>
+                            </select>
+                            <br></br>
+                        </>
+                        : '' 
                     }
+                    {
+                        this.state.animaltype === 'dog' ?
+                        <>
+                            <select id="dog-breed" name="breed" value={this.state.breed} onChange={this.handleChange}>
+                                <option value="">Breed (optional)</option>
+                                <option value="Beagle">Beagle</option>
+                                <option value="Boxer">Boxer</option>
+                                <option value="Chihuahua">Chihuahua</option>
+                                <option value="Retriever">Retriever</option>
+                                <option value="Shepherd">Shepherd</option>
+                                <option value="Terrier">Terrier</option>
+                            </select>
+                            <br></br>
+                        </>
+                        : ''
+                    }
+                    
+                    <select id="gender" name="gender" value={this.state.gender} onChange={this.handleChange}>
+                        <option value="">Gender (optional)</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select>
+                    <br></br>
                     <input type="submit" value="Search" />
                 </form>
             </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Animals from './animal';
+import Form from './form';
 import * as serviceWorker from './serviceWorker';
 
+class App extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        tokenSet: false,
+        token: '',
+        animals: [],
+        searched: false
+      }
+  
+    }
+  
+    getData = (list) => {
+      this.setState({animals: list});
+      this.setState({searched: true});
+    }
+    
+    componentDidMount() {
+        fetch('https://api.petfinder.com/v2/oauth2/token', {
+          method: 'post',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            'client_id': process.env.REACT_APP_API_KEY,
+            'client_secret': process.env.REACT_APP_API_SECRET,
+            'grant_type': 'client_credentials'
+          })
+        })
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({token: data.access_token}); 
+          this.setState({tokenSet: true});
+        })
+        .catch(console.log)
+    }//end componentMount
+  
+    render () {
+      return (
+        <div>
+          <h1>Animals list</h1>
+          <Form token={this.state.token} updateList={this.getData} />
+          {this.state.animals ? <Animals animals={this.state.animals} /> : ''}
+          {this.state.animals.length === 0 && this.state.searched ? <h2>No results found, try changing your search criteria.</h2> : ''}
+        </div>
+      );
+    }
+  }
+  
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
